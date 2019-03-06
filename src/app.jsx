1e65e75659c89af31cc511bf5cc8671a6f2748ef
Jaswanth-Stackroute <jaswanthstackroute@gmail.com>
2019-03-06 14:24:08 +0530

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+      return (
+        <div>
+          <p>Header</p>
+          <p>Content</p>
+          <p>Footer</p>
+        </div>
+      );
+    }
+  }
+  export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// document.body.innerText='hello javascript';
+//document.body.innerText='hello javascript'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src/app.jsx';
+//   const reactElem = React.createElement('h1',null,'helo ReactJs');
+//     ReactDOM.render(reactElem,document.getElementById('root'))   //script for sample h1
+
+
+ReactDOM.render(<App/>,document.getElementById('root'));
